500e08b67f83c38badfb6414b184448bd1d408e4
Ganga sree <gangasree05@gmail.com>
2025-05-15 18:58:49 +0530

    files

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,8 +8,8 @@ const generateTokenAndSetCookie = (userId, res) => {
 	res.cookie("jwt", token, {
 		maxAge: 15 * 24 * 60 * 60 * 1000, // MS
 		httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-		sameSite: "none", // Allow cross-site cookies for frontend/backend on different domains
-		secure: process.env.NODE_ENV !== "development",
+		sameSite: "none", // allow cross-site cookies
+		secure: true, // must be true for SameSite: 'none'
 	});
 };
 
