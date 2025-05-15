2e6b7bc2680fe4b8dda7b4c9159ce343d206d269
Ganga sree <gangasree05@gmail.com>
2025-05-15 15:30:47 +0530

    files

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://mern-chatapplication-1-962t.onrender.com"],
+    origin: ["http://localhost:5173", "https://mern-chat-application-omega.vercel.app"],
     credentials: true,
   })
 );
