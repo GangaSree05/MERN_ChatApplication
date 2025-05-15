c5b3acf526fa7199459c34a6c7e3e6ec82333fca
Ganga sree <gangasree05@gmail.com>
2025-05-15 19:08:49 +0530

    files

--- backend/server.js
+++ backend/server.js
@@ -6,27 +6,25 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
-import { app, server } from "./socket/socket.js";
+import { Server } from "socket.io";
+import http from "http";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-// Middlewares
+// Create express app and apply middleware
+const app = express();
 app.use(express.json());
 app.use(cookieParser());
-// ...existing code...
-
 app.use(cors({
   origin: ['http://localhost:5173', 'https://mern-chat-application-omega.vercel.app'],
   credentials: true
 }));
-// ...existing code...
-
 
 // Test route
 app.get("/", (req, res) => {
-	res.send("Hello from Chat App API!");
+  res.send("Hello from Chat App API!");
 });
 
 // Routes
@@ -34,9 +32,37 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+// Create HTTP server and Socket.io server
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: [
+      "http://localhost:5173",
+      "https://mern-chat-application-omega.vercel.app"
+    ],
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+const userSocketMap = {};
+export const getReceiverSocketId = (receiverId) => userSocketMap[receiverId];
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+  const userId = socket.handshake.query.userId;
+  if (userId != "undefined") userSocketMap[userId] = socket.id;
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  socket.on("disconnect", () => {
+    console.log("user disconnected", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
+
 server.listen(PORT, () => {
-	connectToMongoDB();
-	console.log(`Server Running on port ${PORT}`);
+  connectToMongoDB();
+  console.log(`Server Running on port ${PORT}`);
 });
 
 export default app;
