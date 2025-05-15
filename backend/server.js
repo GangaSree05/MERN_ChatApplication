fe56cf9accc715b955b58a61cc5c2626b5ab3cec
Ganga sree <gangasree05@gmail.com>
2025-05-15 16:01:35 +0530

    files

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 // ...existing code...
-const cors = require('cors');
+
 app.use(cors({
   origin: ['http://localhost:5173', 'https://mern-chat-application-omega.vercel.app'],
   credentials: true
